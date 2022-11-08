9c3141d1ef7528048a5ac7b9eaff36afc2ffb482
ohoherror <1458251589@qq.com>
2022-11-08 17:26:43 +0800

    file

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ohoherror/datafile"
+	"log"
+)
 
 func main() {
-	fmt.Println("bye")
-	fmt.Println(888)
+	numbers, err := datafile.GetFloats("D:\\xc-work\\study\\go\\test\\src\\readfile\\data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Println("Average:%0.2f\n", sum/sampleCount)
 }
