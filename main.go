7620563c06c082da2f276d171c913141b070ef00
ohoherror <1458251589@qq.com>
2022-11-08 13:57:07 +0800

    ddd

--- main.go
+++ main.go
@@ -3,13 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 99.5}
-	var sum float64 = 0
-	for _, number := range numbers {
-		sum += number
-	}
-	sampleCount := float64(len(numbers))
-	fmt.Println("Average: %0.2f\n", sum/sampleCount)
-	fmt.Println("I change the", sum)
-	//fmt.Println(sum)
+
+	fmt.Println(888)
 }
